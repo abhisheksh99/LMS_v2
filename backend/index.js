3ece3dd8b2df04812c45c49fcd9641d62c154f13
abhisheksh99 <abhishekleo1234@gmail.com>
2024-12-25 22:39:19 -0500

    cors added

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
